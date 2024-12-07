6c04bf30ffca1469857426d3384862122346b477
Emerson Matos <emegson@live.com>
2024-12-06 22:26:48 -0300

    migration

--- src/db/utils.rs
+++ src/db/utils.rs
@@ -1,10 +1,21 @@
 use diesel::{r2d2, PgConnection};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
 pub type DbPool = r2d2::Pool<r2d2::ConnectionManager<PgConnection>>;
 
 pub fn get_pool(db_url: &str) -> DbPool {
     let manager = r2d2::ConnectionManager::<PgConnection>::new(db_url);
-    r2d2::Pool::builder()
+
+    let pool = r2d2::Pool::builder()
         .build(manager)
-        .expect("Error building a connection pool")
+        .expect("Error building a connection pool");
+
+    pool.get()
+        .unwrap()
+        .run_pending_migrations(MIGRATIONS)
+        .unwrap();
+
+    pool
 }
